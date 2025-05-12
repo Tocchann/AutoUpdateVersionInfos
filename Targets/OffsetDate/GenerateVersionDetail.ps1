c21f6f5c7807397fd82d5320b8b570fec688dd58
Toshiyuki Takahagi <tosiyuki@hh.iij4u.or.jp>
2025-05-12 11:06:45 +0900

    バージョン管理ロジックの改善とコメント追加
    GenerateVersion.targetsファイルにおいて、バージョン情報の取得や更新に関するコメントを追加し、処理の流れを明確化しました。また、チェックリテールファイルの存在確認やビルド日付の取得ロジックを強化し、バージョン管理の精度を向上させました。
    
    GenerateVersionDetail.ps1ファイルでは、バージョン生成ロジックを改善し、リビジョン番号の更新に関する条件を追加しました。これにより、ビルドの履歴に基づいたバージョン管理がより正確に行えるようになりました。

--- Targets/OffsetDate/GenerateVersionDetail.ps1
+++ Targets/OffsetDate/GenerateVersionDetail.ps1
@@ -11,6 +11,7 @@ param (
 )
 
 begin {
+    # ここでは Major.Minor までが確定していればよいので、あまり細かいことは考えない
     $baseVer = [System.Version]::new($BaseVersionStr)
     if( Test-Path $TargetPath ){
         $currVer = [System.Version]::new([System.Diagnostics.FileVersionInfo]::GetVersionInfo($TargetPath).FileVersion)
@@ -22,19 +23,46 @@ begin {
 }
 
 process {
+    # 今日の日付をもとにビルド番号を決める
     # ビルド番号は、$StartYear 年1月を基準として、経過月*100 + 日 とする
     # 例: $StartYear=2024、ビルド日($today)=2025/04/15 => ((2025-2024)*12+4)*100+15 = 1615
     # 例: $StartYear=2025、ビルド日($today)=2025/04/15 => ((2025-2024)*12+4)*100+15 = 415
     $build = (($today.Year - $StartYear)*12+$today.Month)*100+$today.Day
-    # Major.Minor 部分は$baseVer を優先して組み立てる
-    $returnVer = [System.Version]::new($baseVer.Major, $baseVer.Minor, $build, 0 )
-    # ビルドが同じバージョンの場合は、日付が同じなら、リビジョンを上げる
-    if( $currVer.Major -eq $baseVer.Major -and $currVer.Minor -eq $baseVer.Minor )
+
+    # リビジョンを除いた新旧バージョンを作る
+    $chkNew = [System.Version]::new($baseVer.Major, $baseVer.Minor, $build, 0 )
+    $chkPrev = [System.Version]::new($currVer.Major, $currVer.Minor, $currVer.Build, 0 )
+    # 前回のリビジョン
+    $revision = $currVer.Revision
+    # 前回ビルド時と Major.Minor.Build までが一致する場合(リビジョン更新があるかもしれない)
+    if( $chkNew -eq $chkPrev )
     {
-        if( $build -eq $currVer.Build ){
-            $returnVer = [System.Version]::new($baseVer.Major, $baseVer.Minor, $build, $currVer.Revision + 1)
+        # 公開マーカーファイルパス(ターゲットパスに ".checkRetail" をつける)の作成
+        $checkRetailFile = $TargetPath + ".checkRetail"
+
+        # 前回ビルドしたファイルが残っている(リビルドしていない)
+        if( Test-Path $TargetPath ){
+            # 前回公開時から更新されている場合だけ更新する
+            $buildDate = [System.IO.File]::GetLastWriteTime($TargetPath)
+            if( Test-Path $checkRetailFile ){
+                $releaseDate = [System.IO.File]::GetLastWriteTime($checkRetailFile)
+                if( $buildDate -lt $releaseDate ){
+                    # 公開マーカーファイルのほうが新しい == 公開後はじめてのビルドなので、前回バージョンと異なるものにする
+                    $revision = $currVer.Revision + 1
+                }
+            }
+        }
+        # ターゲットパスがない場合は、リビルドか、意図的に消した場合のいずれかになる。
+        # リテールチェックファイルがある場合(==公開済み)は、更新の必要ありとみなす
+        elseif( Test-Path $checkRetailFile ){
+            $revision = $currVer.Revision + 1
         }
     }
+    # 前回ビルド時と Major.Minor.Build のいずれかが異なる == 前回バージョンとは一致しない == リビジョンはリセットしてよい
+    else{
+        $revision = 0
+    }
+    $returnVer = [System.Version]::new($chkNew.Major, $chkNew.Minor, $chkNew.Build, $revision)
 }
 
 end {
--- Targets/SimpleIncrement/GenerateVersionDetail.ps1
+++ Targets/SimpleIncrement/GenerateVersionDetail.ps1
@@ -9,24 +9,48 @@ param (
 )
 
 begin {
+    # 基準バージョンがどこまで入っているかで本当は状況が変わるけどここでは気にせずトータルでカウントアップになる
     $baseVer = [System.Version]::new($BaseVersionStr)
+    # 前回ビルドしたファイルがある(２回目以降のビルド)
     if( Test-Path $TargetPath ){
         $currVer = [System.Version]::new([System.Diagnostics.FileVersionInfo]::GetVersionInfo($TargetPath).FileVersion)
     }
     else{
         $currVer = [System.Version]::new($PriorVersionStr)
     }
+    # 基準バージョンが変わっているかわからないので、リビジョンだけそろえておく
     $baseVer = [System.Version]::new($baseVer.Major, $baseVer.Minor, $baseVer.Build, $currVer.Revision)
 }
 
 process {
     # 基準は渡されたバージョンに合わせる
     $returnVer = $baseVer
-    # バージョンが同じならリビジョンをカウントアップ
+    $revision = $baseVer.Revision
+    # バージョンが同じ == リビジョンアップの必要がありそう
     if( $currVer -eq $baseVer )
     {
-        $returnVer = [System.Version]::new($currVer.Major, $currVer.Minor, $currVer.Build, $currVer.Revision + 1)
+        # リビジョンの更新は前回ビルド後に公開処理している場合はリビジョンを上げる
+        # それ以外(公開後２回目以降のビルドまたは、そもそもまだ公開していない)は原則バージョン更新はしない
+        $checkRetailFile = $TargetPath + ".checkRetail"
+        if( Test-Path $TargetPath ){
+            # 前回ビルド後に公開処理がおこなわれている場合は更新するので日付情報を確認する
+            $buildDate = [System.IO.File]::GetLastWriteTime($TargetPath)
+            # リテールチェックファイルが有る == 公開済み == インストーラがある == 同日リリースなのでリビジョンアップする
+            if( Test-Path $checkRetailFile ){
+                $releaseDate = [System.IO.File]::GetLastWriteTime($checkRetailFile)
+                if( $buildDate -lt $releaseDate ){
+                    # リリース日付より新しい場合は、リビジョンをインクリメントする
+                    $revision = $currVer.Revision + 1
+                }
+            }
+            # リテールチェックファイルがない == 未公開 == インストーラはまだないので更新は出来るだけおこなわない(意味がない)
+        }
+        # ターゲットパスがない場合は、リビルドか、意図的に消した場合のいずれかになる。この場合は比較条件がないので強制カウントアップ
+        elseif( Test-Path $checkRetailFile ){
+            $revision = $currVer.Revision + 1
+        }
     }
+    $returnVer = [System.Version]::new($baseVer.Major, $baseVer.Minor, $baseVer.Build, $revision)
 }
 
 end {
